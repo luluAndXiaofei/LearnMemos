4c7f8d353ae60952fa4794685a4a86e0f844b2db
RYOKU <ryoku@RYOKUnoMacBook-Pro.local>
2020-07-30 17:24:54 +0900

    lesson7

---
+++ Vue/lesson7/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div style="width:1080px; margin:0 auto">
+      <Banner :banners="banners" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "./components/Banner";
+
+export default {
+  components: {
+    Banner,
+  },
+  data() {
+    return {
+      banners: [
+        { url: require("./assets/banner/banner1.jpeg"), link: "http://www.baidu.com" },
+        { url: require("./assets/banner/banner2.jpeg"), link: "http://www.baidu.com" },
+        { url: require("./assets/banner/banner3.jpeg"), link: "http://www.baidu.com" },
+      ],
+    };
+  },
+};
+</script>
