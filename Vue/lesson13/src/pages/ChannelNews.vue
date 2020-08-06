8404ffe1ca2c1003c28bc41b5cf854826582cee2
RYOKU <ryoku@RYOKUnoMacBook-Pro.local>
2020-08-06 15:37:02 +0900

    lesson13

---
+++ Vue/lesson13/src/pages/ChannelNews.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <Pager :page="page" :limit="10" :total="2011" @pageChange="handleChange" />
+  </div>
+</template>
+
+<script>
+import Pager from "@/components/Pager";
+
+export default {
+  components: {
+    Pager,
+  },
+
+  data() {
+      return {
+          page: 3,
+      };
+  },
+
+  methods: {
+      handleChange(newPage) {
+          this.page = newPage;
+      }
+  }
+};
+</script>
+
+<style></style>
