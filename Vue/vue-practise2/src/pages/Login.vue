9feb1d7a6f7536d4df32ca8a4298bb661a864417
luluAndXiaofei <59049236+luluAndXiaofei@users.noreply.github.com>
2020-08-27 08:58:09 +0900

    update

--- Vue/vue-practise2/src/pages/Login.vue
+++ Vue/vue-practise2/src/pages/Login.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-      login
-  </div>
+  <Center>
+      <div class="login-container">
+        <div class="form-item">
+          <div class="input">
+            <label for="">账号：</label>
+            <input type="text">
+          </div>
+        </div>
+      </div>
+  </Center>
 </template>
 
 <script>
-export default {
+import Center from "@/components/Center";
 
+export default {
+  components: {
+    Center,
+  }
 }
 </script>
 
 <style>
+.login-container {
+  width: 400px;
+}
 
+.form-item .input {
+  display: flex;
+}
+.form-item label {
+  width: 100px;
+  display: inline-block;
+  text-align: right;
+  line-height: 41px;
+}
+
+.form-item input,
+.form-item button {
+  flex-grow: 1;
+}
 </style>
